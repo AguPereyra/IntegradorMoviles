e729585473ab5b268012104c1132fdcad3878de2
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-09-23 08:30:51 -0300

    Image changed to resource id

--- app/src/main/java/com/iua/agustinpereyra/model/Cattle.kt
+++ app/src/main/java/com/iua/agustinpereyra/model/Cattle.kt
@@ -2,4 +2,4 @@ package com.iua.agustinpereyra.model
 
 import android.media.Image
 
-data class Cattle(val caravan: String, val weight: Int, val image: Image)
+data class Cattle(val caravan: String, val weight: Int, val imageId: Int)
