e637f84767ccd2a7cc3e1756d3f2ae3f6b9f78f8
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-09-23 07:47:35 -0300

    Register and Login navigation implemented

--- app/src/main/java/com/iua/agustinpereyra/controller/WelcomeActivity.kt
+++ app/src/main/java/com/iua/agustinpereyra/controller/WelcomeActivity.kt
@@ -8,11 +8,25 @@ import com.iua.agustinpereyra.controller.LoginFragment.loginFragmentListener
 import com.iua.agustinpereyra.utils.USERNAME
 import kotlinx.android.synthetic.main.login_fragment.*
 
-class WelcomeActivity : AppCompatActivity(), loginFragmentListener{
+class WelcomeActivity : AppCompatActivity(), loginFragmentListener, RegisterFragment.registerFragmentListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_activity)
+
+        // Set fragment dinamically
+        //1. Get a reference to fragment manager
+        val fragmentManager = supportFragmentManager
+
+        //2. Start a fragment transaction
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        //3. Add the fragment to the container
+        fragmentTransaction.replace(R.id.welcome_fragment_layout, LoginFragment())
+
+        //4. Commit transaction
+        fragmentTransaction.commit()
+
     }
 
     override fun navigateToMainPage() {
@@ -21,6 +35,21 @@ class WelcomeActivity : AppCompatActivity(), loginFragmentListener{
         val mainPageIntent = Intent(this, CattleListActivity::class.java)
         mainPageIntent.putExtra(USERNAME, username)
         startActivity(mainPageIntent)
-        // TODO: Check why fragment is not showing!
+    }
+
+    override fun nagivateToRegisterPage() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val registerFragment = RegisterFragment()
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.replace(R.id.welcome_fragment_layout, registerFragment)
+        fragmentTransaction.commit()
+
+    }
+
+    override fun nagivateToLoginPage() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val loginFragment = LoginFragment()
+        fragmentTransaction.replace(R.id.welcome_fragment_layout, loginFragment)
+        fragmentTransaction.commit()
     }
 }
