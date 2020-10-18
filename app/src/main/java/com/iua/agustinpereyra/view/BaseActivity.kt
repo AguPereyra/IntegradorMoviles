ddeb82ad0f4cdda9344f7dd3cd8127ef0d4cbaf5
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-10-18 16:42:19 -0300

    Added base activity with simple toolbar

---
+++ app/src/main/java/com/iua/agustinpereyra/view/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.iua.agustinpereyra.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.app_simple_toolbar.*
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setSupportActionBar(app_bar)
+    }
+}
