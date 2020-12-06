720d832916d0f1cd05ebea7173cfa73be404c79d
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-12-06 11:46:23 -0300

    Changed User to Dataclass

--- app/src/main/java/com/iua/agustinpereyra/model/User.kt
+++ app/src/main/java/com/iua/agustinpereyra/model/User.kt
@@ -1,3 +1,3 @@
 package com.iua.agustinpereyra.model
 
-class User(val email: String, val username: String, val passwd: String)
+data class User(val email: String, val username: String, val passwd: String)
