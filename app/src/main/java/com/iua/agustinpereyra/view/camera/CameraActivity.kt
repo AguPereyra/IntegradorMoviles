8916a371f95e3bedb9a37017f6c99acf8b763e81
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-12-14 16:28:39 -0300

    Added camera access

--- app/src/main/java/com/iua/agustinpereyra/view/camera/CameraActivity.kt
+++ app/src/main/java/com/iua/agustinpereyra/view/camera/CameraActivity.kt
@@ -2,6 +2,10 @@ package com.iua.agustinpereyra.view.camera
 
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
@@ -10,10 +14,13 @@ import com.iua.agustinpereyra.controller.CAMERA_PERMISSIONS_CODE
 import com.iua.agustinpereyra.controller.CAMERA_REQUIRED_PERMISSIONS
 import com.iua.agustinpereyra.databinding.ActivityCameraBinding
 import com.iua.agustinpereyra.view.base.BaseActivity
+import java.lang.Exception
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class CameraActivity : BaseActivity() {
+
+    private val TAG = CameraActivity::class.java.name
 
     // Keep track of asynchronous camera's tasks
     private lateinit var cameraExecutor: ExecutorService
@@ -26,7 +33,7 @@ class CameraActivity : BaseActivity() {
 
         // Check for camera permissions and request if needed
         if (allPermissionsGranted()) {
-            // TODO: Implement
+            startCamera()
         } else {
             // Ask for permission
             ActivityCompat.requestPermissions(this, arrayOf(CAMERA_REQUIRED_PERMISSIONS), CAMERA_PERMISSIONS_CODE)
@@ -43,7 +50,7 @@ class CameraActivity : BaseActivity() {
         if (requestCode == CAMERA_PERMISSIONS_CODE) {
             // Check if permissions were granted
             if (allPermissionsGranted()) {
-                // TODO: Implement
+                startCamera()
             } else {
                 // Explain problems of not having camera permissions
                 val snackBar = Snackbar.make(viewBinding.root, getString(R.string.no_camera_permission), Snackbar.LENGTH_INDEFINITE)
@@ -60,5 +67,39 @@ class CameraActivity : BaseActivity() {
     // Function used to check if needed permissions were granted
     private fun allPermissionsGranted() = arrayOf(CAMERA_REQUIRED_PERMISSIONS).all {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    /**
+     * startCamera starts the camera and binds it to the activity's lifecycle
+     */
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener( Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    // Set surface provider on preview
+                    it.setSurfaceProvider(viewBinding.cameraPreview.surfaceProvider)
+                }
+
+            // Set back camera as default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            // Check if nothing is bind to the camera provider
+            // and bind the camera selector and preview to it
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview
+                )
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed while trying to bind CameraX Preview use case", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
     }
 }
